73b93bfe3b14640de96a6268282d372db741f77c
fewizz <hazeevaidar@gmail.com>
2023-01-11 08:00:50 +0300

    return self

--- include/print/buffer.hpp
+++ include/print/buffer.hpp
@@ -18,23 +18,25 @@ public:
 
 	buffer(handle<posix::file> handle) : handle_{ handle } {}
 
-	void flush() {
-		handle_->write_from(buff_);
-		buff_.clear();
-	}
-
 	~buffer() {
 		flush();
 	}
 
+	buffer& flush() {
+		handle_->write_from(buff_);
+		buff_.clear();
+		return *this;
+	}
+
 	template<typename... Types>
 	requires (sizeof...(Types) > 1)
-	void operator() (Types&&... values) {
+	buffer& operator() (Types&&... values) {
 		((*this)(forward<Types>(values)), ...);
+		return *this;
 	}
 
 	template<basic_range Range>
-	void operator () (Range&& r) {
+	buffer& operator () (Range&& r) {
 		auto iterator = range_iterator(r);
 		auto elements_to_put = range_size(r);
 
@@ -55,14 +57,16 @@ public:
 				iterator, iterator + elements_to_put
 			}.as_range()
 		);
+		return *this;
 	}
 
 	template<nuint Size>
-	void operator () (const char (&str_array)[Size]) {
+	buffer& operator () (const char (&str_array)[Size]) {
 		(*this)(c_string{ str_array });
+		return *this;
 	};
 
-	void number(unsigned_integer auto number, number_base base = 10) {
+	buffer& number(unsigned_integer auto number, number_base base = 10) {
 		nuint count = 0;
 		::number{ number}.for_each_digit(base, [&](nuint) { ++count; });
 		char digits[count];
@@ -71,18 +75,21 @@ public:
 			digits[count++] = (char)digit + (digit <= 9 ? '0' : 'A' - 10);
 		});
 		(*this)(span{ digits, count });
+		return *this;
 	}
 
-	void operator () (unsigned_integer auto number) {
+	buffer& operator () (unsigned_integer auto number) {
 		this->number(number, 10);
+		return *this;
 	}
 
-	void hex(unsigned_integer auto number) {
+	buffer& hex(unsigned_integer auto number) {
 		(*this)("0x");
 		this->number(number, 16);
+		return *this;
 	}
 
-	void operator () (signed_integer auto number) {
+	buffer& operator () (signed_integer auto number) {
 		if(number < 0) {
 			(*this)("-");
 			(*this)((nuint)-number);
@@ -90,28 +97,32 @@ public:
 		else {
 			(*this)((nuint)number);
 		}
+		return *this;
 	};
 
-	void operator () (float number) {
+	buffer& operator () (float number) {
 		(*this)((int32)number);
 		(*this)(".");
 		float fract =
 			::number{        number }.absolute() -
 			::number{ (int32)number }.absolute();
 		(*this)((nuint) (fract * 10000.0F));
+		return *this;
 	}
 
-	void operator () (double number) {
+	buffer& operator () (double number) {
 		(*this)((int64)number);
 		(*this)(".");
 		double fract =
 			::number{        number }.absolute() -
 			::number{ (int64)number }.absolute();
 		(*this)((nuint) (fract * 100000000.0));
+		return *this;
 	}
 
-	void operator () (bool b) {
+	buffer& operator () (bool b) {
 		b ? (*this)("true") : (*this)("false");
+		return *this;
 	}
 
 
